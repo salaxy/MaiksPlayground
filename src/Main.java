8a3a84fe943097a62b4deddf652850d2c220995c
Salaxy <salaxy@web.de>
2016-12-29 00:16:58 +0100

    - neues feature xy

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,14 @@ public class Main {
 		
 		System.out.println("Test123");
 
+		Main.neueMethode();
+		
+	}
+	
+	public static void neueMethode(){
+		
+		
+		System.out.println("zweite ausgabe");
 	}
 
 }
